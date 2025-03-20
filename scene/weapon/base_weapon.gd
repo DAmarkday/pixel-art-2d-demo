3503f71e87a5d5f1bd1abba74f67ed2626fb041d
xiejinqiao <damarkday@163.com>
2025-03-20 23:58:16 +0800

    feat:+1.添加特效

--- scene/weapon/base_weapon.gd
+++ scene/weapon/base_weapon.gd
@@ -4,20 +4,23 @@ class_name BaseWeapon
 const _pre_bullet = preload("res://scene/bullet/BaseBullet.tscn")
 
 @onready var bullet_point = $BulletPoint
+@onready var audio2d = $AudioStreamPlayer2D
 
 @export var bullet_max = 30
 @export var damage = 5
 @export var weapon_rof = 0.2
 @export var weapon_name = '默认枪械'
+@onready var fire_particles = $GPUParticles2D
 var current_rof_tick = 0
 
 @onready var sprite = $Sprite2D
 
-var current_bullet_count = 0
+var current_bullet_count = 0 
 
 var player:Player
 
 func _ready():
+	fire_particles.lifetime = weapon_rof - 0.01
 	current_bullet_count = bullet_max
 	PlayerManager.on_weapon_changed.emit(self)
 	PlayerManager.on_bullet_count_changed.emit(current_bullet_count,bullet_max)
@@ -32,9 +35,20 @@ func shoot():
 	
 	current_bullet_count -=1
 	PlayerManager.on_bullet_count_changed.emit(current_bullet_count,bullet_max)
-	
 	if current_bullet_count <=0:
 		reload()
+	weapon_anim()
+
+func weapon_anim():
+	fire_particles.restart()
+	
+	var tween = create_tween().set_ease(Tween.EASE_IN)
+	tween.tween_property(sprite,'scale:x',0.7,weapon_rof / 2)
+	tween.tween_property(sprite,'scale:x',1,weapon_rof / 2)
+	if audio2d:
+		audio2d.play()
+	
+	pass
 
 func reload():
 	PlayerManager.on_weapon_reload.emit()
